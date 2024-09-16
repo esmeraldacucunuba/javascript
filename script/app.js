7295010570847303ce48da621235948bdbbb52be
esmeraldacucunuba <esmeraldacucunuba@yahoo.es>
2024-09-16 00:25:58 -0500

    repaso javascript

---
+++ script/app.js
@@ -0,0 +1,55 @@
+// comentario de una linea
+
+/**comentario multilinea
+ */ 
+
+console.log("hola mundo");
+
+//variables
+
+let numero1 = 45;
+let numero2 = 39;
+let suma, resta, multiplicacion;
+
+const pi= 3.1416;
+let estado = true
+
+let numero3 = 78;
+
+numero3= 125;
+
+let direccion= "calle 11 # 40-69";
+
+
+console.log(numero1);
+console.log(numero2);
+console.log(pi)
+console.log(estado)
+console.log(numero3)
+console.log(direccion)
+
+suma =numero1 + numero2;
+resta =numero1 - numero2;
+multiplicacion =numero1 * numero2;
+
+
+console.log(suma)
+
+
+//concatenar valores
+
+console.log( 'EL PRIMER VALOR ES: ' + numero1);
+console.log( 'EL SEGUNDO VALOR ES: ' + numero2);
+
+console.log( 'LA SUMA DE ' + numero1 + ' Y ' + numero2 + ' ES: ' + suma);
+console.log( 'LA SUMA DE ' , numero1 , ' Y ' , numero2 , ' ES: ' , suma);
+
+//literal templete
+console.log( `LA SUMA DE ${numero1}  Y ${numero2}  ES: ${suma}`);
+console.log( `LA RESTA DE ${numero1}  Y ${numero2}  ES: ${resta}`);
+console.log( `LA MULTIPLICACION DE ${numero1}  Y ${numero2}  ES: ${multiplicacion}`);
+
+
+
+
+
